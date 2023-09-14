d170670085b1373db98c733b02d9f07f81c9244d
Matej3 <matopucik@gmail.com>
2023-09-14 19:48:29 +0200

    init

---
+++ src/main/kotlin/14.9.2023.kt
@@ -0,0 +1,48 @@
+fun main(){
+
+    val white_pieces = "♖♘♗♔♕♗♘♖"
+    val white_pawns =  "♙♙♙♙♙♙♙♙"
+    val black_pieces = "♜♞♝♛♚♝♞♜"
+    val black_pawns =  "♟♟♟♟♟♟♟♟"
+    val white_empty =  "   #  #  #  #"
+    val black_empty =  " #  #  #  #  "
+
+    println(black_pieces)
+    println(black_pawns)
+    println(white_empty)
+    println(black_empty)
+    println(white_empty)
+    println(black_empty)
+    println(white_pawns)
+    println(white_pieces)
+
+}
+
+
+
+
+/*
+import java.util.Scanner
+import kotlin.math.sin
+
+fun main() {
+    val text = "svet"
+
+    print("Hello World!") // vypis bez endline
+
+    println("ahoj" + 123 + "$text" ) // vypis s endline, prvy munsi byt text, da sa spajat cez +
+
+    val cin = Scanner(System.`in`) // vpis
+
+    cin.nextLine()
+
+    val x = 1 // neda sa nijako menit
+    var y = 2 // da sa menit pocas programnu, napr ++, y = 20 etc.
+    val z : Int = 2 // da sa urcit typ premennej, vo vacsine casu netreba
+
+    val k = sin(215.6) // sam doimportuje co treba
+    print("$cin" + k )
+
+
+}
+*/
