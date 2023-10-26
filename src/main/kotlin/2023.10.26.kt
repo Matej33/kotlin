22c932d35a13f2b7f83da83c89a8e74d09f389d6
Matej Púčik <matej.pucik@student.ssnd.sk>
2023-10-26 15:02:42 +0200

    update git ignore

---
+++ src/main/kotlin/2023.10.26.kt
@@ -0,0 +1,10 @@
+class Url(val scheme: String = "https", val host: String, val path: String , val query: String) {
+    fun getUrl(): String {
+        return "$scheme://$host/$path?$query"
+    }
+}
+
+fun main(){
+    val url = Url("https", "www.ssnd.sk", "api", "getCuricullum")
+    println(url.getUrl())
+}
